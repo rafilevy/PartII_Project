caae8c6a2a1764d40c2fd15146f843764492af0b
Rafi Levy <rafi.levy@sky.com>
2022-04-26 14:41:47 +0100

    Increased data ammount

--- experiments/feature_tests/LoRaWAN_data_cost/main.py
+++ experiments/feature_tests/LoRaWAN_data_cost/main.py
@@ -35,6 +35,7 @@ s.setblocking(False)
 
 while True:
     for i in range(10):
+        print("Sending", (i+1) * 20, "bytes")
         s.send( bytes([rand_byte() for _ in range((i+1) * 25)] ) )
 
         #Flash LED green and sleep for .5 seconds either side
